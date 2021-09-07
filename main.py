95659cfda1e4477f229be9b9205e07ea4b8af87c
Henry Tang <hktang@ualberta.ca>
2021-09-07 15:30:10 -0600

    Cleaned up the linking of paragraphs and code examples
    * Added fuzzywuzzy matching for candidate paragraphs
    
    * Made constants for file names
    
    * Added code comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import functools
 import subprocess
 import os
 from fuzzywuzzy import fuzz
@@ -5,19 +6,26 @@ import re
 from pyquery import PyQuery as pq
 
 
-# P_REGEX = re.compile("(?<=<p>).+?(?=</p>)")
-# CODE_REGEX = re.compile("(?<=<code>)[\s\S]*?(?=</code>)")
+LINKS_FILE = "results/links.txt"
+TASKS_FILE = "results/tasks.txt"
+
+
+def call_extractor(inp):
+    inp = inp.replace("’", "'")
+    result = subprocess.run(["java", "-jar", "StringToTasks.jar", inp.encode("utf-8").decode("utf-8")], stdout=subprocess.PIPE)
+    return result.stdout.decode("utf-8")
+
 
 def get_paragraphs_and_tasks(paragraphs):
     paragraphs_w_tasks = []
-    with open("tasks.txt", "w", encoding="utf-8") as task_file:
+    with open(TASKS_FILE, "w", encoding="utf-8") as task_file:
         os.chdir("TaskExtractor")
         for paragraph in paragraphs.items():
             if len(paragraph[0].classes) == 0:
                 extract = call_extractor(paragraph.text())
                 if extract:
-                    paragraphs_w_tasks.append(paragraph.text())
-                    task_file.write(paragraph.text())
+                    paragraphs_w_tasks.append(paragraph.text().strip())
+                    task_file.write(paragraph.text().strip())
                     task_file.write("\n")
                     task_file.write(extract.replace("\r\n", "\n"))
                     task_file.write("\n")
@@ -26,35 +34,36 @@ def get_paragraphs_and_tasks(paragraphs):
     return paragraphs_w_tasks
 
 
+def fuzzy_compare(potential, paragraph):
+    return fuzz.partial_ratio(potential, paragraph)
+
+
 def link_code_examples_and_paragraphs(code_examples, paragraphs):
-    with open("links.txt", "w", encoding="utf-8") as links_file:
+    with open(LINKS_FILE, "w", encoding="utf-8") as links_file:
         for example in code_examples.items():
-            code = example
-            # Look for the closest parent with siblings
-            while example.siblings().length == 0:
-                example = example.parent()
+            if example.parent()[0].tag != "p":
+                code = example
+                # Look for the closest parent with siblings
+                while example.siblings().length == 0:
+                    example = example.parent()
 
-            # Find the paragraph that is right above the code example
-            paragraph = None
-            for child in example.parent().children():
-                if re.match(re.compile("h[0-6]"), child.tag):
-                    paragraph = None
-                # TODO: There is an issue where the paragraphs list contains paragraphs with inline code
-                # But child.text does not contain inline code. So there are false links
-                # That is okay because we do not care about inline
-                elif child.text in paragraphs:
-                    paragraph = child
-                if example[0] == child:
-                    break
-            if paragraph is not None:
-                links_file.write(paragraph.text + "\n")
-                links_file.write(code.text() + "\n\n")
-
-
-def call_extractor(inp):
-    inp = inp.replace("’", "'")
-    result = subprocess.run(["java", "-jar", "StringToTasks.jar", inp.encode("utf-8").decode("utf-8")], stdout=subprocess.PIPE)
-    return result.stdout.decode("utf-8")
+                # Find the paragraph that is right above the code example not crossing a header
+                paragraph = None
+                for child in example.parent().children():
+                    # If the current child is a header then any potential paragraph previously should not be relevant
+                    if re.match(re.compile("h[0-6]"), child.tag):
+                        paragraph = None
+                    elif child.text and child.text.count(" ") > 2:
+                        for i, ratio in enumerate(list(map(functools.partial(fuzzy_compare, child.text.strip()), paragraphs))):
+                            if ratio == 100:
+                                paragraph = paragraphs[i]
+                    # If we reach the code example then break, since according to our heuristic,
+                    # explanations are not below examples
+                    if example[0] == child:
+                        break
+                if paragraph is not None:
+                    links_file.write(paragraph + "\n")
+                    links_file.write(code.text() + "\n\n")
 
 
 def main():
