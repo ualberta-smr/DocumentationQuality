217059a4b00d2b31a831065a3651bab87732700d
Henry Tang <hktang@ualberta.ca>
2021-11-24 16:51:20 -0700

    Added website snapshots

---
+++ TaskExtractor/Snapshots of HTML/nltk-parse_files/documentation_options.js
@@ -0,0 +1,47 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.6',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
+
+}
+/*
+     FILE ARCHIVED ON 04:38:25 Apr 18, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:38:30 Oct 26, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  PetaboxLoader3.resolve: 152.679
+  load_resource: 1243.29
+  LoadShardBlock: 127.45 (3)
+  PetaboxLoader3.datanode: 1171.092 (4)
+  esindex: 0.009
+  exclusion.robots: 0.142
+  exclusion.robots.policy: 0.133
+  cdx.remote: 0.061
+  CDXLines.iter: 17.453 (3)
+  captures_list: 1602.448
+*/
---
+++ TaskExtractor/Snapshots of HTML/nltk-tag_files/documentation_options.js
@@ -0,0 +1,47 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.6',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
+
+}
+/*
+     FILE ARCHIVED ON 00:38:45 Jul 25, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:38:34 Oct 26, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 314.924
+  exclusion.robots: 0.142
+  exclusion.robots.policy: 0.129
+  RedisCDXSource: 3.78
+  esindex: 0.009
+  LoadShardBlock: 289.417 (3)
+  PetaboxLoader3.datanode: 219.627 (4)
+  CDXLines.iter: 16.704 (3)
+  load_resource: 49.401
+  PetaboxLoader3.resolve: 38.24
+*/
