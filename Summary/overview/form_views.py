55bacd205317e432e70d2c62b81ac34d929bf98e
Henry Tang <hktang@ualberta.ca>
2022-10-20 14:23:11 -0600

    Reverted demographics form

--- Summary/overview/form_views.py
+++ Summary/overview/form_views.py
@@ -102,27 +102,12 @@ def create(request):
                   context={"form": form, "groupings": get_groupings()})
 
 
-def demographics_form(request, library_name):
+def demographics_form(request):
     if request.method == "POST":
         form = Demographics(request.POST)
         if form.is_valid():
             form.save()
-        if not request.session.exists(request.session.session_key):
-            request.session.create()
-            print(
-                "--------------------------------------------------------------------------------Created Session in demographics------------------------------------------------------------------")
-        if "store" not in request.session:
-            request.session["store"] = initialize_store(
-                request.session.session_key,
-                library_name)
-            print(
-                "--------------------------------------------------------------------------------Created store in demographics------------------------------------------------------------------")
-        data = {"session_key": request.session["store"]["session_key"],
-                "library_name": request.session["store"]["library_name"],
-                "familiar": form.cleaned_data["familiar"] if "familiar" in form.cleaned_data else "",
-                "years_experience": form.cleaned_data["years_experience"] if "years_experience" in form.cleaned_data else ""
-                }
-        request.session["store"]["demographics_form"] = json.dumps(data)
+        request.session["store"]["demographics_form"] = json.dumps(form.cleaned_data)
         request.session.modified = True
     return redirect("overview:overview", request.POST["library_name"])
 
