5b30be3852e7add7f2993aace0f5e1f7e325ae7c
Henry Tang <hktang@ualberta.ca>
2022-10-20 10:45:03 -0600

    Changed name of app to docqual

--- Summary/summary/urls.py
+++ Summary/summary/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('overview/', include('overview.urls')),
+    path('docquality/', include('overview.urls')),
 ]
