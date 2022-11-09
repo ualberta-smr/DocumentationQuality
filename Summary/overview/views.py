a583fc719a3fccecc262da0ff0d799a46e70272d
Henry Tang <hktang@ualberta.ca>
2022-11-09 10:38:58 -0700

    Added missing file

--- Summary/overview/views.py
+++ Summary/overview/views.py
@@ -32,19 +32,19 @@ def overview(request, library_name):
     try:
         response = Response.objects.get(library_name=library_name,
                                         session_key=request.session.session_key)
-        show_form = False
+        show_demographic_form = False
         familiar = response.familiar
-        try:
-            if not request.session["store"]["survey_form"]:
-                form = dict()
-                for field in response._meta.get_fields():
-                    if field.name != "id":
-                        form[field.name] = getattr(response, field.name)
-                request.session["store"]["survey_form"] = json.dumps(form)
-        except MultiValueDictKeyError:
-            pass
+        # try:
+        #     if not request.session["store"]["survey_form"]:
+        #         form = dict()
+        #         for field in response._meta.get_fields():
+        #             if field.name != "id":
+        #                 form[field.name] = getattr(response, field.name)
+        #         request.session["store"]["survey_form"] = json.dumps(form)
+        # except MultiValueDictKeyError:
+        #     pass
     except Response.DoesNotExist:
-        show_form = True
+        show_demographic_form = True
         familiar = False
 
     values = {
@@ -59,7 +59,7 @@ def overview(request, library_name):
         "consistency": library_metrics.consistency_ratio,
         "navigability": library_metrics.navigability_score,
         "familiar": familiar,
-        "show_form": show_form,
+        "show_demographic_form": show_demographic_form,
         "survey_form": request.session["store"]["survey_form"],
         "demographics_form": request.session["store"]["demographics_form"],
         # "general_form": request.session["store"]["general_form"],
