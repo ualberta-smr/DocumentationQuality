bbffad9a3edcccc4a60bb3eb8b1778f250d2c8fc
sarah-afiya <sarah.afiya@gmail.com>
2023-08-29 02:57:55 -0600

    Rename class

---
+++ documentation_quality_analysis/analyze_library/models/matched_function.py
@@ -0,0 +1,10 @@
+from documentation_quality_analysis.analyze_library.models.Signature import Signature
+from documentation_quality_analysis.analyze_library.models.doc_code_example import DocCodeExample
+
+
+class MatchedCall:
+    def __init__(self, called_signature: Signature, raw_example: DocCodeExample, original_call: str, url: str):
+        self.called_signature: Signature = called_signature
+        self.raw_example: DocCodeExample = raw_example
+        self.original_call: str = original_call
+        self.url = url
