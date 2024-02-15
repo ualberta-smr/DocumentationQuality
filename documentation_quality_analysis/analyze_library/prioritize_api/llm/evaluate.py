419be7e4407b04a9719b210785ab1930e58c5843
sarah-afiya <sarah.afiya@gmail.com>
2024-02-15 05:30:22 -0700

    evaluate on 5 pandas APIs

---
+++ documentation_quality_analysis/analyze_library/prioritize_api/llm/evaluate.py
@@ -0,0 +1,60 @@
+import csv
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score
+
+from analyze_library.prioritize_api.llm.util import preprocess_response
+
+CWD = os.getcwd()
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+RESPONSE_PATH = os.path.join(ROOT_DIR, "query_responses")
+EVAL_DATA_PATH = os.path.join(ROOT_DIR, "evaluate")
+
+
+def evaluate_result():
+    library = "pandas"
+
+    gt_path = os.path.join(CWD, f"prioritize_api/evaluation/libraries/{library}/discusses_api_ground_truth")
+    response_dir_path = os.path.join(RESPONSE_PATH, library)
+    eval_data_dir_path = os.path.join(EVAL_DATA_PATH, library)
+
+    results = [['API', 'Num Example', 'Recall', 'Accuracy', 'Precision', 'F-measure']]
+    df_title = pd.DataFrame(columns=['id', 'DISCUSSES_API'])
+
+    with open(os.path.join(gt_path, "selected_apis_for_ground_truth.csv")) as f:
+        apis = f.read().split()
+
+    for api in apis:
+        gt_data = pd.read_csv(os.path.join(gt_path, f"{api}.csv"))
+        df_values = gt_data.loc[:, ['id', 'DISCUSSES_API']]
+        eval_df = pd.concat([df_title, df_values])
+        eval_df["result"] = np.nan
+
+        res_data = pd.read_csv(os.path.join(response_dir_path, f"{api}.csv"))
+
+        for id in res_data['id'].values:
+            print(id)
+            response = res_data[res_data['id'] == id]['response'].values[0]
+
+            result = preprocess_response(response)
+
+            eval_df.loc[eval_df['id'] == id, ['result']] = result
+
+        recall = recall_score(list(eval_df['DISCUSSES_API']), list(eval_df['result']))
+        precision = precision_score(list(eval_df['DISCUSSES_API']), list(eval_df['result']), zero_division=1)
+        accuracy = accuracy_score(list(eval_df['DISCUSSES_API']), list(eval_df['result']))
+        f_measure = f1_score(list(eval_df['DISCUSSES_API']), list(eval_df['result']), zero_division=1)
+
+        results.append([api, len(eval_df), recall, accuracy, precision, f_measure])
+
+        eval_df.to_csv(os.path.join(eval_data_dir_path, f"{api}.csv"), sep=',')
+
+    with open(os.path.join(eval_data_dir_path, "eval_result.csv"), 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(results)
+
+
+if __name__ == '__main__':
+    evaluate_result()
