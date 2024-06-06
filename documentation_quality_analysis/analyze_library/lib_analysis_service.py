e93fb1ee5ff7525f2bf34114bce5c405d2ca9894
sarah-afiya <sarah.afiya@gmail.com>
2024-06-06 11:31:59 -0600

    refactor

--- documentation_quality_analysis/analyze_library/lib_analysis_service.py
+++ documentation_quality_analysis/analyze_library/lib_analysis_service.py
@@ -1,9 +1,12 @@
+import os
 import sys
 from typing import List, Union
 
+import pandas as pd
+
 from analyze_library.db_service.db_service import save_lib_overview_to_db
 from analyze_library.doc_parser.doc_parser import get_all_webpages, \
-    get_functions_and_classes_from_doc_api_ref, get_functions_and_classes_from_doc_examples
+    get_functions_and_classes_from_doc_api_ref, get_code_examples_from_doc
 from analyze_library.models.Signature import Signature
 from analyze_library.models.doc_code_example import DocCodeExample
 from analyze_library.models.doc_page import DocPage
@@ -18,9 +21,9 @@ from analyze_library.source_parser.source_code_parser import \
     get_methods_and_classes_from_source_code
 from analyze_library.source_parser.source_util import clone_repo
 from analyze_library.stack_overflow_service.stack_overflow_service import get_mentions_in_stack_overflow, \
-    analyze_SO_posts
+    get_SO_posts_for_APIs_wo_eg
 from analyze_library.stats.stats import get_stats_ex_per_api, get_stats_api_per_example, \
-    write_stats_to_file, write_doc_api_to_csv, write_examples_to_csv, get_stats_ex_per_methods_and_classes, \
+    write_api_per_example_stats_to_file, write_doc_api_to_csv, write_examples_to_csv, get_stats_ex_per_methods_and_classes, \
     write_examples_per_api_stats_to_file
 from analyze_library.metric_calculation.text_readability.text_readability import get_text_readability
 from analyze_library.metric_calculation.metrics import Metrics
@@ -30,6 +33,7 @@ sys.path.append("...")
 depth_map = {
     'requests': 2,
     'pandas': 3,
+    'scipy': 4,
     'graphql compiler': 3,
     'collections': 0,
     'flask': 2,
@@ -60,12 +64,13 @@ def analyze_library(language, library_name, doc_url, gh_url) -> LibraryOverview:
 
     doc_pages: List[DocPage] = get_all_webpages(doc_url, depth)
 
+    # ToDo: need to refactor get_functions_and_classes_from_doc_api_ref --> should not get pd.cut or empty
     doc_apis: List[Union[Signature, None]] = get_functions_and_classes_from_doc_api_ref(doc_pages)
 
     matched_signatures, unmatched_signatures = match_doc_api_to_src_api(library_name, mapped_source_api,
                                                                         doc_apis=doc_apis)
 
-    doc_code_examples: List[DocCodeExample] = get_functions_and_classes_from_doc_examples(doc_pages)
+    doc_code_examples: List[DocCodeExample] = get_code_examples_from_doc(doc_pages)
 
     matched_methods: List[MatchedCall] = match_examples_to_doc_api(library_name, doc_code_examples, doc_apis)
 
@@ -73,12 +78,28 @@ def analyze_library(language, library_name, doc_url, gh_url) -> LibraryOverview:
     stats_example_per_method, stats_example_per_class = get_stats_ex_per_methods_and_classes(doc_apis, matched_methods)
 
     stats_api_per_example = get_stats_api_per_example(matched_methods, doc_code_examples)
+
+    write_api_per_example_stats_to_file(stats_api_per_example=stats_api_per_example,
+                                        lib_name=library_name)
     write_examples_per_api_stats_to_file(stats_example_per_api, library_name)
+
     unmatched_signature_string = ', '.join([i.fully_qualified_name for i in unmatched_signatures])
 
+    print(str(len(doc_apis)))
+
     if doc_apis:
+        CWD = os.getcwd()
+        dir_path = os.path.join(CWD, f"prioritize_api/evaluation/libraries/{library_name}")
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        path = os.path.join(dir_path, f"{library_name}_apis_wo_eg.csv")
+
         apis_with_no_example = [i for i in stats_example_per_api if i and not stats_example_per_api[i]]
-        analyze_SO_posts(lib_name=library_name, apis=apis_with_no_example)
+        df_apis_with_no_example = pd.DataFrame({'APIs_with_no_example': apis_with_no_example})
+        df_apis_with_no_example.to_csv(path, encoding='ascii', errors='replace', index=False, header=False)
+
+        print("saved apis with no examples: " + path)
 
     metrics = Metrics(example_per_method=stats_example_per_method,
                       example_per_class=stats_example_per_class,
@@ -106,11 +127,11 @@ def analyze_library(language, library_name, doc_url, gh_url) -> LibraryOverview:
         navigability_score=metrics.navigability,
         general_rating=metrics.general_rating)
 
-    if doc_apis:
-        save_lib_overview_to_db(lib_overview)
+    # if doc_apis:
+    #     save_lib_overview_to_db(lib_overview)
 
-    else:
-        print("Got 0 doc APIs for " + library_name)
+    # else:
+    #     print("Got 0 doc APIs for " + library_name)
 
     print(f'Done analyzing {library_name}')
 
@@ -130,10 +151,21 @@ def get_depth(lib_name):
 
 
 if __name__ == '__main__':
+    # analyze_library("python", "pandas",
+    #                 "https://pandas.pydata.org/docs/index.html",
+    #                 "https://github.com/pandas-dev/pandas")
 
-    analyze_library("python", "requests",
-                    "https://requests.readthedocs.io/en/latest/",
-                    "https://github.com/psf/requests.git")
+    analyze_library("python", "scipy",
+                        "https://docs.scipy.org/doc/scipy/",
+                        "https://github.com/scipy/scipy")
+
+    # analyze_library("python", "requests",
+    #                 "https://requests.readthedocs.io/en/latest/",
+    #                 "https://github.com/psf/requests.git")
+    #
+    # analyze_library("python", "boto3",
+    #                 "https://requests.readthedocs.io/en/latest/",
+    #                 "https://github.com/psf/requests.git")
 
     # analyze_library("python", "ibis",
     #                 "https://ibis-project.org/",
